8117b21a1a71de6ff1e8a06ca1bb00e22c7a718f
Kyle King <kyle.king@quentustech.com>
2012-04-13 11:59:12 -0700

    working log for CHANNEL_CREATE, CHANNEL_ANSWER, and CHANNEL_APPLICATION

--- mod_fail2ban.c
+++ mod_fail2ban.c
@@ -8,82 +8,50 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_fail2ban_load);
  */
 SWITCH_MODULE_DEFINITION(mod_fail2ban, mod_fail2ban_load, mod_fail2ban_shutdown, NULL);
 
-char *logfile = "/var/log/freeswitch.log";
+switch_memory_pool_t *modpool;
+char *logfile_name = "/usr/local/freeswitch/log/fail2ban.log";
+switch_file_t *logfile;
+switch_event_types_t event = SWITCH_EVENT_ALL;
+const char *subclass_name = SWITCH_EVENT_SUBCLASS_ANY;
 
 static switch_status_t mod_fail2ban_do_config(void);
 
-static switch_xml_t mod_fail2ban_xml_search(const char *section, const char *tag_name, const char *key_name, const char *key_value, switch_event_t *params,
-										void *user_data)
-{
-	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "XML_SEARCH CALLED!\n");
-	
-	return NULL;
-}
-
-static void shutdown_event_handler(switch_event_t *event)
+static void event_handler(switch_event_t *event)
 {
 
 	if (event->event_id == SWITCH_EVENT_CHANNEL_CREATE) {
-
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "event Channel Hangup was called\n");
+		switch_file_printf(logfile, "event Channel create was called\n");
 
 	} else if (event->event_id == SWITCH_EVENT_CHANNEL_ANSWER) { 
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "event answer was called\n");
+		switch_file_printf(logfile, "event answer was called\n");
 
 	} else if (event->event_id == SWITCH_EVENT_CHANNEL_APPLICATION) { 
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "event application was called\n");
+		switch_file_printf(logfile, "event application was called\n");
 
 	}
 }
 
-static void shutdown_event_handler(switch_file_t *file, char *log_data)
-{
-	switch_size_t len;
-	switch_status_t status = SWITCH_STATUS_SUCCESS;
-	len = strlen(log_data);
-	
-	if (switch_file_write(file, log_data, &len) != SWITCH_STATUS_SUCCESS) {
-		switch_file_close(profile->log_afd);
-		if ((status = mod_logfile_openlogfile(profile, SWITCH_TRUE)) == SWITCH_STATUS_SUCCESS) {
-			len = strlen(log_data);
-			switch_file_write(profile->log_afd, log_data, &len);
-		}
-	}
-	return status;
-}
-
-
+// SWITCH_DECLARE(int) switch_file_printf(switch_file_t *thefile, const char *format, ...);
 
 /* Macro expands to: switch_status_t mod_fail2ban_load(switch_loadable_module_interface_t **module_interface, switch_memory_pool_t *pool) */
 SWITCH_MODULE_LOAD_FUNCTION(mod_fail2ban_load)
 {
-	switch_status_t status;
-	switch_event_types_t event = SWITCH_EVENT_ALL;
-	const char *subclass_name = SWITCH_EVENT_SUBCLASS_ANY;
 	void *user_data = NULL;
+	switch_status_t status;
 	*module_interface = switch_loadable_module_create_module_interface(pool, modname);
+	modpool = pool;
 
-	if ((status = switch_event_bind(modname, event, subclass_name, shutdown_event_handler, user_data)) != SWITCH_STATUS_SUCCESS) {
+	if ((status = switch_event_bind(modname, event, subclass_name, event_handler, user_data)) != SWITCH_STATUS_SUCCESS) {
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "event bind failed\n");
+		return SWITCH_STATUS_FALSE;
 	} else {
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "event bind success\n");
 	}
 
-
-	/* Set the xml search callback. */
-	
-	if ((status = switch_xml_bind_search_function(mod_fail2ban_xml_search, switch_xml_parse_section_string("directory"), NULL)) != SWITCH_STATUS_SUCCESS) {
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "xml search failed\n");
-	} else {
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "xml search success\n");
-	}
-
-	
 	if (mod_fail2ban_do_config() != SWITCH_STATUS_SUCCESS) {
 		return SWITCH_STATUS_FALSE;
 	}
 
-	/* indicate that the module should continue to be loaded */
 	return SWITCH_STATUS_SUCCESS;
 }
 
@@ -92,38 +60,62 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_fail2ban_load)
   Macro expands to: switch_status_t mod_fail2ban_shutdown() */
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_fail2ban_shutdown)
 {
+	void *user_data = NULL;
+	switch_status_t status;
+	switch_event_node_t *node;
 
-	return SWITCH_STATUS_SUCCESS;
+	if ((status = switch_event_bind_removable(modname, event, subclass_name, event_handler, user_data, &node)) != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "event bind failed\n");
+		return SWITCH_STATUS_SUCCESS;
+	} else {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "event bind success\n");
+		return SWITCH_STATUS_FALSE;
+	}
 }
 
 static switch_status_t mod_fail2ban_do_config(void)
 {
-	char *cf = "logfile.conf";
+	switch_status_t status;
+	char *cf = "fail2ban.conf";
 	switch_xml_t cfg, xml, bindings_tag, config = NULL, param = NULL;
+	char *bname;
 	char *var;
 	char *val;
+	
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "setting configs\n");		
 
 	if (!(xml = switch_xml_open_cfg(cf, &cfg, NULL))) {
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Open of %s failed\n", cf);
 		return SWITCH_STATUS_TERM;
 	}
-
-	if (!(bindings_tag = switch_xml_child(cfg, "settings"))) {
+	
+	if (!(bindings_tag = switch_xml_child(cfg, "bindings"))) {
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Missing <bindings> tag!\n");
 		goto done;
 	}
-	for (param = switch_xml_child(config, "param"); param; param = param->next) {
-		var = (char *) switch_xml_attr_soft(param, "name");
-		val = (char *) switch_xml_attr_soft(param, "value");
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "%s: %s\n", var, val);		
+	
+	for (config = switch_xml_child(bindings_tag, "config"); config; config = config->next) {
+		bname = (char *) switch_xml_attr_soft(config, "name");
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "bname: %s\n",bname);		
 		
-		if (strncmp(var,"logfile", 7) == 0) {
-			logfile = val;
-		} else if (strncmp(var,"rotate-on-hup",13) == 0) {
-		} else {
-			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Unknown attribute %s: %s\n", var, val);		
-		}
-	}	
+		for (param = switch_xml_child(config, "param"); param; param = param->next) {
+			var = (char *) switch_xml_attr_soft(param, "name");
+			val = (char *) switch_xml_attr_soft(param, "value");
+			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "%s: %s\n", var, val);		
+			
+			if (strncmp(var,"logfile", 7) == 0) {
+				logfile_name = val;
+			} else {
+				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Unknown attribute %s: %s\n", var, val);		
+			}
+		}	
+	}
+	
+	
+	if ((status = switch_file_open(&logfile, logfile_name, SWITCH_FOPEN_WRITE, SWITCH_FPROT_UWRITE, modpool)) != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "failed to open %s\n", logfile_name);		
+		return SWITCH_STATUS_FALSE;
+	}
 	
  done:
 	switch_xml_free(xml);
